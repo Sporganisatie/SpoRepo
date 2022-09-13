2032360db57a137dcfb77950b00a529ad8534b4f
Rens Reus <r.reus@tjip.com>
2022-09-13 11:05:11 +0200

    disable swagger on prod

--- Program.cs
+++ Program.cs
@@ -19,14 +19,14 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseMiddleware<JwtMiddleware>();
 
