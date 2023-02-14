6a2e1ecbc6a2874e96cf3dc989c4373ccfc0e8cf
Rens Reus <rensreus@gmail.com>
2023-02-14 19:13:21 +0100

    comment

--- api/Infrastructure/Clients/AccountClient.cs
+++ api/Infrastructure/Clients/AccountClient.cs
@@ -9,7 +9,7 @@ public class AccountClient
     }
 
     public Task<Result<Account>> Get(string email)
-        => Result.For(DatabaseContext.Accounts.Single(x => x.Email.Equals(email))).AsTask(); // TODO hoe moet dit met Result
+        => Result.For(DatabaseContext.Accounts.Single(x => x.Email.Equals(email))).AsTask(); // TODO hoe moet dit met Result, we willen errors afvangen zonder overal try/catch
 
     public Task<Result<Account>> Get(int id)
         => Result.For(DatabaseContext.Accounts.Single(x => x.AccountId == id)).AsTask();
