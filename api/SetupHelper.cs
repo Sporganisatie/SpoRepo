483c2d53e6e353e92b27f1a59b8e3d4c9311ce32
Rens Reus <rensreus@gmail.com>
2023-02-14 19:58:35 +0100

    move setup

--- api/SetupHelper.cs
+++ api/SetupHelper.cs
@@ -10,7 +10,6 @@ internal static class Telemetry
     {
         services.AddScoped<AccountService, AccountService>();
         services.AddScoped<AccountClient, AccountClient>();
-        services.AddScoped<DatabaseContext, DatabaseContext>();
     }
 
     public static void AddSwaggerLogin(this IServiceCollection services)
