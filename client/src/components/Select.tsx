02ff4a00d5aa8f5477cac8936f9671c72e419f2c
Rens Reus <rensreus@gmail.com>
2023-04-30 14:14:31 +0200

    Filter, select components

---
+++ client/src/components/Select.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+export interface SelectOption<T extends string | number> {
+    displayValue: string;
+    value: T;
+}
+
+export interface SelectProps<T extends string | number> {
+    options: SelectOption<T>[];
+    onChange: (selectedOption: T) => void;
+    value?: T;
+}
+
+function Select<T extends string | number>(props: SelectProps<T>) {
+    const { options, onChange, value: initialValue } = props;
+
+    const [value, setValue] = useState(initialValue);
+
+    useEffect(() => {
+        setValue(initialValue);
+    }, [initialValue]);
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedValue = event.target.value as unknown as T;
+        onChange(selectedValue);
+        setValue(selectedValue);
+    };
+
+    return (
+        <select onChange={handleChange} value={value}>
+            {options.map((option, index) => (
+                <option key={index} value={option.value}>
+                    {option.displayValue}
+                </option>
+            ))}
+        </select>
+    );
+}
+
+export default Select;
--- client/src/pages/teamselection/Teamselection.tsx
+++ client/src/pages/teamselection/Teamselection.tsx
@@ -7,14 +7,7 @@ import SelectableRidersTable from './SelectableRidersTable';
 import TeamSelectionTable from './TeamSelectionTable';
 import { RiderParticipation } from '../../models/RiderParticipation';
 import { useBudgetContext } from '../../components/shared/BudgetContextProvider';
-
-interface Filters {
-    name: string,
-    minPrice: string,
-    maxPrice: string,
-    team: string,
-    skill: string,
-};
+import FilterElements, { Filters } from './Filters';
 
 const Teamselection: React.FC = () => {
     let { raceId } = useParams();
@@ -26,8 +19,8 @@ const Teamselection: React.FC = () => {
     function getDefaulFilterState(): Filters {
         return {
             name: "",
-            minPrice: "500000",
-            maxPrice: "7000000",
+            minPrice: 500000,
+            maxPrice: 7000000,
             team: "",
             skill: ""
         }
@@ -37,6 +30,8 @@ const Teamselection: React.FC = () => {
             ...filters,
             ...part
         }
+        newFilter.minPrice = Math.min(newFilter.minPrice, newFilter.maxPrice)
+        newFilter.maxPrice = Math.max(newFilter.minPrice, newFilter.maxPrice)
         setFilters(newFilter)
         setFilteredRiders(filterRiders(newFilter, data.allRiders));
     }
@@ -96,71 +91,7 @@ const Teamselection: React.FC = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
             <div>Budget Over: {data.budgetOver / 1_000_000}M/{data.budget / 1_000_000}M</div>
-            <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem", margin: "1rem" }}>
-                <input
-                    type="text"
-                    value={filters.name}
-                    placeholder="naam"
-                    onChange={(e) => updateAndFilter({ name: e.target.value })}
-                />
-                <select value={filters.minPrice} onChange={(e) => {
-                    const maxPrice = Math.max(Number(e.target.value), Number(filters.maxPrice));
-                    updateAndFilter({ minPrice: e.target.value, maxPrice: maxPrice.toString() });
-                }}>
-                    <option value="500000">500.000</option>
-                    <option value="750000">750.000</option>
-                    <option value="1000000">1.000.000</option>
-                    <option value="1500000">1.500.000</option>
-                    <option value="2000000">2.000.000</option>
-                    <option value="2500000">2.500.000</option>
-                    <option value="3000000">3.000.000</option>
-                    <option value="3500000">3.500.000</option>
-                    <option value="4000000">4.000.000</option>
-                    <option value="4500000">4.500.000</option>
-                    <option value="5000000">5.000.000</option>
-                    <option value="5500000">5.500.000</option>
-                    <option value="6000000">6.000.000</option>
-                    <option value="7000000">7.000.000</option>
-                </select>
-                <select value={filters.maxPrice} onChange={(e) => {
-                    const minPrice = Math.min(Number(e.target.value), Number(filters.minPrice));
-                    updateAndFilter({ maxPrice: e.target.value, minPrice: minPrice.toString() });
-                }}>
-                    <option value="500000">500.000</option>
-                    <option value="750000">750.000</option>
-                    <option value="1000000">1.000.000</option>
-                    <option value="1500000">1.500.000</option>
-                    <option value="2000000">2.000.000</option>
-                    <option value="2500000">2.500.000</option>
-                    <option value="3000000">3.000.000</option>
-                    <option value="3500000">3.500.000</option>
-                    <option value="4000000">4.000.000</option>
-                    <option value="4500000">4.500.000</option>
-                    <option value="5000000">5.000.000</option>
-                    <option value="5500000">5.500.000</option>
-                    <option value="6000000">6.000.000</option>
-                    <option value="7000000">7.000.000</option>
-                </select>
-                <select value={filters.skill} onChange={(e) => {
-                    updateAndFilter({ skill: e.target.value });
-                }}>
-                    <option value=""></option>
-                    <option value="gc">Klassement</option>
-                    <option value="sprint">Sprint</option>
-                    <option value="climb">Klimmen</option>
-                    <option value="tt">Tijdrijden</option>
-                    <option value="punch">Punch</option>
-                </select>
-                <input
-                    type="text"
-                    placeholder="teamnaam"
-                    value={filters.team}
-                    onChange={(e) => updateAndFilter({ team: e.target.value })}
-                />
-                <button onClick={() => resetFilter()}>
-                    Reset
-                </button>
-            </div>
+            <FilterElements updateFilter={updateAndFilter} resetFilter={resetFilter} filters={filters} />
             <div style={{ display: "flex" }}>
                 <SelectableRidersTable
                     data={filteredRiders}
@@ -192,10 +123,10 @@ function filterRiders(filters: Filters, riders: SelectableRider[]): SelectableRi
                 );
                 break;
             case "minPrice":
-                riders = riders.filter(({ details }) => details.price >= Number(value));
+                riders = riders.filter(({ details }) => details.price >= value);
                 break;
             case "maxPrice":
-                riders = riders.filter(({ details }) => details.price <= Number(value));
+                riders = riders.filter(({ details }) => details.price <= value);
                 break;
             case "team":
                 riders = riders.filter(({ details }) => details.team.toLowerCase().includes(value.toLowerCase()));
