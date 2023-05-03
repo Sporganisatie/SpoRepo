4e4e78a5dce3ed9c496ab147a0bbd0b5ae10daa5
Rens Reus <rensreus@gmail.com>
2023-05-03 20:45:12 +0200

    show 0 stars as -

--- client/src/pages/teamselection/SelectableRiderFoldOut.tsx
+++ client/src/pages/teamselection/SelectableRiderFoldOut.tsx
@@ -2,11 +2,12 @@ import { ExpanderComponentProps } from "react-data-table-component";
 import { SelectableRider } from "./Models/SelectableRider";
 
 const SelectableRiderFoldout: React.FC<ExpanderComponentProps<SelectableRider>> = ({ data }) => <div>
-    {data.details.gc > 0 && <div>Klassement:{'★'.repeat(data.details.gc)}</div>}
-    {data.details.sprint > 0 && <div>Sprint:{'★'.repeat(data.details.sprint)}</div>}
-    {data.details.climb > 0 && <div>Klimmen:{'★'.repeat(data.details.climb)}</div>}
-    {data.details.tt > 0 && <div>Tijdrijden:{'★'.repeat(data.details.tt)}</div>}
-    {data.details.punch > 0 && <div>Punch:{'★'.repeat(data.details.punch)}</div>}
+    {<div>Klassement: {data.details.gc > 0 ? '★'.repeat(data.details.gc) : "-"}</div>}
+    {<div>Sprint: {data.details.sprint > 0 ? '★'.repeat(data.details.sprint) : "-"}</div>}
+    {<div>Klimmen: {data.details.climb > 0 ? '★'.repeat(data.details.climb) : "-"}</div>}
+    {<div>Tijdrijden: {data.details.tt > 0 ? '★'.repeat(data.details.tt) : "-"}</div>}
+    {<div>Punch: {data.details.punch > 0 ? '★'.repeat(data.details.punch) : "-"}</div>}
+    <br></br>
 </div>;
 
 
