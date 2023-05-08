6602f81d7fac75cbd809abc1dd5b6893e7c1796d
Rens Reus <rensreus@gmail.com>
2023-05-08 10:34:12 +0200

    unused

--- api/Infrastructure/Database/Clients/RaceClient.cs
+++ api/Infrastructure/Database/Clients/RaceClient.cs
@@ -11,9 +11,6 @@ public class RaceClient
     public bool StageStarted(int raceId, int stagenr)
         => DateTime.UtcNow >= DatabaseContext.Stages.Single(x => x.RaceId == raceId && x.Stagenr == stagenr).Starttime;
 
-    public Stage StageInfo(int raceId, int stagenr)
-        => DatabaseContext.Stages.Single(x => x.RaceId == raceId && x.Stagenr == stagenr);
-
     public int CurrentStage(int raceId) // afhankelijk van finished/complete maken
         => DatabaseContext.Stages.Where(s => s.RaceId == raceId).ToList().OrderByDescending(s => s.Starttime).First(s => s.Starttime < DateTime.UtcNow)?.Stagenr ?? 0;
 }
