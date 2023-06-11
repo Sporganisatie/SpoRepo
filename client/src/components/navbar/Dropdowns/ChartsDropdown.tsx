21f8f23ed6736efe49537514e83424b2641bae9e
Rens Reus <rensreus@gmail.com>
2023-06-11 14:56:53 +0200

    Bijna hele mooie animatie

--- client/src/components/navbar/Dropdowns/ChartsDropdown.tsx
+++ client/src/components/navbar/Dropdowns/ChartsDropdown.tsx
@@ -10,7 +10,6 @@ const ChartsDropdown = (props: { raceSelected: boolean }) => {
       ]}
       raceOnlyLinks={[
         { url: "/charts/scoreverloop/27", title: "Relatief Scoreverloop" }, // TODO dynamic race id
-        { url: "/charts/positieverloop/27", title: "Positie Scoreverloop" }, // TODO dynamic race id
         { url: "/charts/scoreverdeling/27", title: "Score Verdeling" }, // TODO dynamic race id
         // { url: "/charts/userscores", title: "Relatief Scoreverloop" },
         // { url: "/charts/userrank", title: "Ranking" },
--- client/src/pages/Charts/ScoreVerloopChart.tsx
+++ client/src/pages/Charts/ScoreVerloopChart.tsx
@@ -15,55 +15,75 @@ const ScoreVerloopChart = () => {
     let { raceId } = useParams();
     const budgetParticipation = useBudgetContext();
     const [chartdata, setChartData] = useState<ChartData>({ data: [], usernames: [] });
+    const [positieVerloop, setPositieVerloop] = useState<boolean>(false);
 
     useEffect(() => {
-        axios.get(`/api/Charts/scoreVerloop`, { params: { raceId, budgetParticipation } })
+        var url = positieVerloop ? "positieVerloop" : "scoreVerloop";
+        axios.get(`/api/Charts/${url}`, { params: { raceId, budgetParticipation } })
             .then(res => {
                 const usernames = res.data[0].usernamesAndScores.map((x: { username: string; }) => x.username);
-                setChartData({ data: convertData(res.data), usernames });
+                setChartData({ data: convertData(res.data, positieVerloop), usernames });
             })
             .catch(error => {
             });
-    }, [raceId, budgetParticipation]);
+    }, [raceId, budgetParticipation, positieVerloop]);
+
+    const togglePositieVerloop = () => {
+        setPositieVerloop(!positieVerloop)
+    }
 
     return (
         <div style={{ backgroundColor: '#222', padding: '20px' }}>
-            <LineChart width={chartdata.data.length * 70} height={600} data={chartdata.data}>
-                <CartesianGrid vertical={false} strokeDasharray="3 3" />
-                <XAxis dataKey="name" >
-                    <Label
-                        value="Etappe"
-                        position="bottom"
-                        dy={-15}
+            <div style={{ cursor: 'pointer', color: 'white  ' }} onClick={togglePositieVerloop}> Positie Verloop
+                <input type="checkbox" checked={positieVerloop} onChange={() => { }} /></div>
+            <div>
+                <LineChart width={Math.min(chartdata.data.length * 70, 1540)} height={600} data={chartdata.data}>
+                    <CartesianGrid vertical={false} strokeDasharray="3 3" />
+                    <XAxis dataKey="name" >
+                        <Label
+                            value="Etappe"
+                            position="bottom"
+                            dy={-15}
+                        />
+                    </XAxis>
+                    {positieVerloop ? (
+                        <YAxis
+                            tickCount={10}
+                            domain={['dataMin - 0.5', 'dataMax + 0.5']}
+                            tickFormatter={(value) => (-value).toString()}
+                            ticks={Array.from({ length: chartdata.usernames.length }, (_, index) => -(index + 1))}
+                        >
+                            <Label value="Positie" angle={-90} position="left" offset={-10} />
+                        </YAxis>
+                    ) : (
+                        <YAxis tickCount={10}>
+                            <Label
+                                value={positieVerloop ? "Positie" : "Relatieve Punten"}
+                                angle={-90}
+                                position="left"
+                                offset={-10}
+                            />
+                        </YAxis>
+                    )}
+                    <Tooltip
+                        contentStyle={{ backgroundColor: '#333', border: 'none' }}
+                        labelStyle={{ color: '#fff' }} />
+                    <Legend
+                        verticalAlign="top"
+                        wrapperStyle={{ marginTop: -10 }}
                     />
-                </XAxis>
-                <YAxis tickCount={10}>
-                    <Label
-                        value="Relatieve Punten"
-                        angle={-90}
-                        position="left"
-                        offset={-10}
-                    />
-                </YAxis>
-                <Tooltip
-                    contentStyle={{ backgroundColor: '#333', border: 'none' }}
-                    labelStyle={{ color: '#fff' }} />
-                <Legend
-                    verticalAlign="top"
-                    wrapperStyle={{ marginTop: -10 }}
-                />
-                {chartdata.usernames.map((username, index) => (
-                    <Line
-                        key={index}
-                        type="linear"
-                        dataKey={username}
-                        stroke={colors[index]}
-                        strokeWidth={3}
-                        dot={false}
-                        isAnimationActive={false}
-                    />
-                ))}
-            </LineChart>
+                    {chartdata.usernames.map((username, index) => (
+                        <Line
+                            key={index}
+                            type="linear"
+                            dataKey={username}
+                            stroke={colors[index]}
+                            strokeWidth={3}
+                            dot={false}
+                        />
+                    ))}
+                </LineChart>
+            </div>
         </div>
     );
 };
