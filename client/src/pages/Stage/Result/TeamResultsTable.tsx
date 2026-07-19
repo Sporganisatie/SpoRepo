a4b2b7bccaddd9c4894398be92378fd0d69ea2ec
Rens Reus <rensreus@gmail.com>
2026-07-19 18:10:40 +0200

    Positie leidend

--- client/src/pages/Stage/Result/TeamResultsTable.tsx
+++ client/src/pages/Stage/Result/TeamResultsTable.tsx
@@ -18,7 +18,7 @@ export type RiderScore = z.infer<typeof riderScoreSchema>;
 const TeamResultsTable = ({ data }: { data: RiderScore[] }) => (
   <Table data={data}>
     {(col) => [
-      col.text(r => r.dnf ? "DNF" : (r.stagePos ? `${r.stagePos}e` : ""), { name: "Positie", width: "12%" }),
+      col.text(r => r.stagePos ? `${r.stagePos}e` : (r.dnf ? "DNF" : ""), { name: "Positie", width: "12%" }),
       col.rider(r => r.rider, { kopman: r => r.kopman, fallback: "Totaal", width: "35%" }),
       col.text(r => r.stageScore, { name: "Dag", width: "12%" }),
       col.text(r => r.classificationScore, { name: "Klassementen", width: "17%" }),
